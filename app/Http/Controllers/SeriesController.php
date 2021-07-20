2f9f1ba0edcc070c984869eae1369234db47d711
MRonald <ronaldmichael918@gmail.com>
2021-07-20 14:03:57 -0300

    Adicionando e listando elementos do banco dinamicamente

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,16 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
     public function index(Request $request) {
-        $series = [
-            'Grey\'s Anatomy',
-            'Lost',
-            'Agents of SHIELD',
-        ];
+        $series = Serie::all();
 
         return view('series.index', compact('series'));
     }
@@ -19,5 +16,13 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nome = $request->nome;
+        $serie = new Serie();
+        $serie->nome = $nome;
+        $serie->save();
     }
 }
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -6,6 +6,7 @@
 
 @section('conteudo')
     <form method="POST">
+        @csrf
         <div class="form-group">
             <label for="nome">Nome:</label>
             <input type="text" class="form-control" name="nome" id="nome"/>
