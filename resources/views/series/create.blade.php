c423fb945c8d8c2cecc48d9e97363047c717881c
MRonald <ronaldmichael918@gmail.com>
2021-07-20 17:13:09 -0300

    Adicionando validaçao do formulario de inserçao

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -5,6 +5,15 @@
 @endsection
 
 @section('conteudo')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul style="list-style: none; margin: 0; padding: 0;">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form method="POST">
         @csrf
         <div class="form-group">
